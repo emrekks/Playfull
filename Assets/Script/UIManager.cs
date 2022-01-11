8f90f217a3fe5ec2d1bd5e9dbc31b0b3432292cf
Emir <emirsavas@windowslive.com>
2022-01-11 15:34:54 +0300

    Visual Update

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,29 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [Header("UI Variables")] 
     public GameObject uiIcons;
     public GameObject gameScene;
-    
-    
-    
+    public GameObject emailButtonObject;
+    public GameObject newsButtonObject;
+    //PrivateVariables
+    private Image emailImage;
+    private Button emailButton;
+    private Image newsImage;
+    private Button newsButton;
+
+    private void Start()
+    {
+        emailImage = emailButtonObject.GetComponent<Image>();
+        emailButton = emailButtonObject.GetComponent<Button>();
+        newsImage = newsButtonObject.GetComponent<Image>();
+        newsButton = newsButtonObject.GetComponent<Button>();
+        emailImage.color = emailButton.colors.pressedColor;
+    }
+
     public void OnClickNews()
     {
         uiIcons.SetActive(false);
         gameScene.SetActive(false);
+        emailImage.color = emailButton.colors.normalColor;
+        newsImage.color = newsButton.colors.pressedColor;
     }
     
     public void OnClickEmail()
     {
         uiIcons.SetActive(true);
         gameScene.SetActive(true);
+        emailImage.color = emailButton.colors.pressedColor;
+        newsImage.color = newsButton.colors.normalColor;
     }
 
     public void OnClickQuitButton()
     {
         Application.Quit();
     }
+    
+    
 }
