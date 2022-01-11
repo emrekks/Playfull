d544b90e05feea392561e5869a0414bc17730be6
Emir <emirsavas@windowslive.com>
2022-01-11 14:47:11 +0300

    News And Email Scene Change

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("UI Variables")] 
+    public GameObject uiIcons;
+    public GameObject gameScene;
+    
+    
+    
+    public void OnClickNews()
+    {
+        uiIcons.SetActive(false);
+        gameScene.SetActive(false);
+    }
+    
+    public void OnClickEmail()
+    {
+        uiIcons.SetActive(true);
+        gameScene.SetActive(true);
+    }
+}
